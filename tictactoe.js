e706de169d9af2d5546b401340c7f2a825cb8eb4
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-06-28 22:05:43 -0400

    Added winChecker() function that takes the array board and checks columns, rows and diagonals to see if X or O appear in succession, if yes then the game returns the winner and stops execution. If not, the game returns null which means game is still on.

--- tictactoe.js
+++ tictactoe.js
@@ -9,7 +9,7 @@ function createPlayer(name) {
     }
 
     function getScore() {
-        return score;
+        return `${name}'s currnet score is ${score}`;
     }
 
     return {
@@ -19,9 +19,64 @@ function createPlayer(name) {
     };
 };
 
-const A = createPlayer("A");
-const B = createPlayer("B");
 
-A.increaseScore();
-console.log(A.getScore());
-console.log(B.getScore());
+// Helper function to check for winning row, column, diagonals
+
+function checkLine(line) {
+    if (line[0] !== "" && line[0] === line[1] && line[1] === line[2]) {
+        return line[0];
+    }
+    return null;
+}
+
+
+
+function winChecker(board) {
+
+    //checking for rows
+
+    for (let i = 0; i < board.length; i++) {
+        const line = board[i];
+        const winner = checkLine(line);
+
+        if (winner !== null) {
+            return winner;
+        }
+    }
+
+    // checking for columns
+
+    for (let i = 0; i < board.length; i++) {
+        const line = [board[0][i], board[1][i], board[2][i]];
+
+        const winner = checkLine(line);
+        if (winner !== null) {
+            return winner;
+        }
+    }
+
+    const line1 = [board[0][0], board[1][1], board[2][2]];
+    const line2 = [board[0][2], board[1][1], board[2][0]];
+
+    const winner1 = checkLine(line1);
+    const winner2 = checkLine(line2);
+
+    if (winner1 !== null) {
+        return winner1;
+    }
+
+    if (winner2 !== null) {
+        return winner2;
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            if (board[i][j] === "") {
+                return null;
+            }
+        }
+    }
+    return "Draw";
+
+}
+
