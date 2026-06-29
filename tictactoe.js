43a3abdbdb0564e46746879ae3add8dfe9780a90
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-06-28 23:08:14 -0400

    Added a result checker function which checks whether the returned result is a X or an O or a draw, if it is X and O, which are symbols occupied by created players stored in players array, it will ask the player who owns it to increase the score. This is working accordingly.

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,9 @@
-const board = [["", "", ""], ["", "", ""], ["", "", ""],];
+/*const board = [["", "", ""], ["", "", ""], ["", "", ""],];*/
+
+const board = [["X", "X", "X"], ["X", "O", "O"], ["O", "X", "O"]]
 
 
-function createPlayer(name) {
+function createPlayer(name, symbol) {
     let score = 0;
 
     function increaseScore() {
@@ -9,15 +11,23 @@ function createPlayer(name) {
     }
 
     function getScore() {
-        return `${name}'s currnet score is ${score}`;
+        return `${name}'s current score is ${score}`;
     }
 
     return {
         name,
+        symbol,
         increaseScore,
         getScore
     };
 };
+
+// Put this inside the game control function later. We are only creating and storing player names and symbols here.
+
+const players = [
+    createPlayer("player 1", "X"),
+    createPlayer("player 2", "O")
+];
 
 
 // Helper function to check for winning row, column, diagonals
@@ -44,7 +54,7 @@ function winChecker(board) {
         }
     }
 
-    // checking for columns
+    // Checking for columns
 
     for (let i = 0; i < board.length; i++) {
         const line = [board[0][i], board[1][i], board[2][i]];
@@ -54,6 +64,8 @@ function winChecker(board) {
             return winner;
         }
     }
+
+    // Checking for diagonals
 
     const line1 = [board[0][0], board[1][1], board[2][2]];
     const line2 = [board[0][2], board[1][1], board[2][0]];
@@ -69,6 +81,9 @@ function winChecker(board) {
         return winner2;
     }
 
+
+    // Checking if there are no winners and if any empty boxes exist in the game
+
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
             if (board[i][j] === "") {
@@ -77,6 +92,31 @@ function winChecker(board) {
         }
     }
     return "Draw";
+};
 
-}
+// Maybe use this function to update UI, turns, reset game on UI
+
+function winHandler() {
+    const result = winChecker(board);
+    return result;
+};
+
+
+// Increases a score for player based on the winner X or O. 
+// This function loops through players array to assign score to the player who owns the winning symbol.
+function resultChecker(result) {
+    for (let player of players) {
+        if (result === player.symbol) {
+            player.increaseScore()
+        }
+    }
+};
+
+const result = winHandler();
+resultChecker(result);
+
+players[0].getScore();
+players[1].getScore();
+
+
 
